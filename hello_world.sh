d75affa25f7df027b3c9c797928ab3a9b88d1548
root <root@ip-172-31-1-142.ap-northeast-2.compute.internal>
2020-05-21 09:19:28 +0000

    Add

--- hello_world.sh
+++ hello_world.sh
@@ -1,8 +1,8 @@
 mpirun -n 4\
-    --hostfile ~/setup/hostfile \
+    --hostfile ~/cloud/hostfile \
     --mca btl_tcp_if_include ens3 --allow-run-as-root \
     -bind-to none -map-by slot \
-    -mca plm_rsh_args "-p 12345"
+    -mca plm_rsh_args "-p 12345" \
     #-x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
     #-mca pml ob1 -mca btl ^openib \
     ~/cloud/mpitutorial/tutorials/mpi-hello-world/code/mpi_hello_world
