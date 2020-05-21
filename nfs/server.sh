875ef2a2b2ccaad67fb53d2d131dcfc58e413104
Ubuntu <ubuntu@ip-172-31-10-29.ap-northeast-2.compute.internal>
2020-05-21 23:39:07 +0000

    Add

--- nfs/server.sh
+++ nfs/server.sh
@@ -4,7 +4,6 @@ sudo apt remove -y nfs-kernel-server
 sudo apt install -y nfs-kernel-server
 
 mkdir $HOME/cloud
-mkdir $HOME/setup
 
 sudo bash -c "echo $HOME/cloud *\(rw,sync,no_root_squash,no_subtree_check\) >> /etc/exports"
 sudo exportfs -a
