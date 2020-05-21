45c45c86d5ca5a1f273768d6c4510c9285518e66
Ubuntu <ubuntu@ip-172-31-10-29.ap-northeast-2.compute.internal>
2020-05-21 07:06:30 +0000

    Add

--- nfs/server.sh
+++ nfs/server.sh
@@ -7,5 +7,4 @@ mkdir $HOME/cloud
 mkdir $HOME/setup
 
 sudo bash -c "echo $HOME/cloud *\(rw,sync,no_root_squash,no_subtree_check\) >> /etc/exports"
-sudo bash -c "echo $HOME/setup *\(rw,sync,no_root_squash,no_subtree_check\) >> /etc/exports"
 sudo exportfs -a
